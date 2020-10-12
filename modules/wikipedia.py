72c2abf59a39ee20e48388724a0c48f59003e921
Sumangalam Avtar <sumangalam_avtar@yahoo.com>
2020-10-12 12:22:15 +0530

    Removed unnecessary requests from Wikipedia module Reduces overall stress on the module
    Signed-off-by: Sumangalam Avtar <sumangalam_avtar@yahoo.com>

--- modules/wikipedia.py
+++ modules/wikipedia.py
@@ -14,29 +14,22 @@ def wikiSearch(query):
 
 
 def random_knowledge():
-    speak("Sir, do you wanna know something from my brains?")
-    choice_to_read = takeCommand()
-    if 'yes' in choice_to_read:
-        page_to_be_provided = wikipedia.random(1)
-        speak("Ok Sir! It's about" + page_to_be_provided)
-        speak("Wanna read??")
-        print(page_to_be_provided)
-        read_y_or_n = takeCommand()
-        if 'yes' in read_y_or_n:
-            load_page = wikipedia.page(page_to_be_provided)
-            speak('Ok Sir! Here\'s the summary')
-            print(load_page.summary)
-            speak(load_page.summary)
-            time.sleep(1)
-            speak("Do you want me to open the article for you to read?")
-            if_open = takeCommand()
-            if 'yes' in if_open:
-                webbrowser.open(load_page.url, new=2)
-                speak("Here's the requested page Sir!")
-            if 'no' in if_open:
-                speak("Ok Sir!")
-        if 'no' in read_y_or_n:
-            speak("Ok Sir! Would tune your interests")
-
-    if 'no' in choice_to_read:
-        speak("Ok sir!")
+    page_to_be_provided = wikipedia.random(1)
+    speak("Sir! Do you want to read about" + page_to_be_provided)
+    print(page_to_be_provided)
+    read_y_or_n = takeCommand()
+    if 'yes' in read_y_or_n:
+        load_page = wikipedia.page(page_to_be_provided)
+        speak('Ok Sir! Here\'s the summary')
+        print(load_page.summary)
+        speak(load_page.summary)
+        time.sleep(1)
+        speak("Do you want me to open the article for you to read?")
+        if_open = takeCommand()
+        if 'yes' in if_open:
+            webbrowser.open(load_page.url, new=2)
+            speak("Here's the requested page Sir!")
+        if 'no' in if_open:
+            speak("Ok Sir!")
+    if 'no' in read_y_or_n:
+        speak("Ok Sir!")
