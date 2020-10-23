5744f7e58a90e2da5058f658a4a0363351337d81
Sumangalam Avtar <sumangalam_avtar@yahoo.com>
2020-10-24 00:49:19 +0530

    Fixed Issue #1 os.path.dirname returned empty, added abspath to fix issue
    Signed-off-by: Sumangalam Avtar <sumangalam_avtar@yahoo.com>

--- setup_files/set_threshold.py
+++ setup_files/set_threshold.py
@@ -1,6 +1,6 @@
 import os
 
-threshold_loc = os.path.normpath((os.path.join(os.path.dirname(__file__), "user/settings")))
+threshold_loc = os.path.normpath((os.path.join(os.path.dirname(os.path.abspath((__file__))), "user/settings")))
 threshold_loc = threshold_loc.replace("\\setup_files", "")
 threshold = input("Set threshold value here! Value should be between 500 to 5000. \nFor optimal results keep value between 2750 to 4000\n Enter your threshold value -\n")
 threshold = int(threshold)
